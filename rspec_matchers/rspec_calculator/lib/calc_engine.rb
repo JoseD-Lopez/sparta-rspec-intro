40da0cce55ec808b0b878586e273b11c4cb3d72a
Tech-A11 <tech-a11@Tech-A11s-MacBook-Air.local>
2018-04-09 20:13:40 +0100

    made the specs and methods to test them and wrote up the readme file

--- rspec_matchers/rspec_calculator/lib/calc_engine.rb
+++ rspec_matchers/rspec_calculator/lib/calc_engine.rb
@@ -1,6 +1,18 @@
 class Calcengine
 
 	def add(number1, number2)
-	number1 + number2
+		number1 + number2
+	end
+
+	def sub(number1, number2)
+		number1 - number2
+	end
+
+	def mult(number1, number2)
+		number1 * number2
+	end
+
+	def div(number1, number2)
+		number1 / number2
 	end
 end
--- rspec_matchers/rspec_calculator/spec/calc_engine_spec.rb
+++ rspec_matchers/rspec_calculator/spec/calc_engine_spec.rb
@@ -8,9 +8,19 @@ describe Calcengine do
 	
 	end
 
-	it 'should correctly add two numbers' do
+	it 'should correctly subtract two numbers' do
 	  calc = Calcengine.new
 	  expect(calc.sub(3,1)).to eql(2)
+	end
+
+	it 'should correctly multiply two numbers' do
+	  calc = Calcengine.new
+	  expect(calc.mult(3,1)).to eql(3)
+	end
+
+	it 'should correctly divide two numbers' do
+	  calc = Calcengine.new
+	  expect(calc.div(10,2)).to eql(5)
 	end
 	
 end
